6b4af37b6ebc06b1aa3f44a50f4b75ef24c1e116
Rake11 <rakeshdasari39@gmail.com>
2025-03-30 13:00:09 +0530

    updating render deploy url  in Frontend

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -23,6 +23,7 @@ const __dirname = path.resolve();
 app.use(express.json({ limit: "500mb" })); // Increase limit for JSON payloads
 app.use(express.urlencoded({ limit: "500mb", extended: true })); // Increase limit for URL-encoded payloads
 app.use(cookieParser());
+
 app.use(cors({
     origin: "http://localhost:5173",
     credentials: true,
--- Frontend/myapp/src/lib/axios.js
+++ Frontend/myapp/src/lib/axios.js
@@ -1,6 +1,15 @@
+// import axios from "axios";
+
+// export const axiosInstance = axios.create({
+//   baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://chat-app-3qsw.onrender.com",
+//   withCredentials: true,
+// });
+
+
+
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5001/api",
   withCredentials: true,
 });
